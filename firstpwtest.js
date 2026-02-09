986a3817448c97638ed10ac5d64b0225907fc8e0
Mohanraj <mohanmraj@rocketmail.com>
2026-02-09 13:44:04 +0530

    From VS code Local to Git hub first commit

---
+++ firstpwtest.js
@@ -0,0 +1,33 @@
+// const { webkit } = require('playwright');
+
+// (async () => {
+//   const browser = await webkit.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto('https://www.poketmohan4.poket.com/admin', { waitUntil: 'networkidle' });
+//   await page.screenshot({ path: 'ex.png', fullPage: true });
+
+//   await browser.close();
+// })();
+
+
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // Open page
+  await page.goto('https://poketmohan4.poket.app/member/', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  // Wait so you can SEE the page
+  await page.waitForTimeout(5000);
+
+  // Take screenshot
+  await page.screenshot({ path: 'ex.png', fullPage: true });
+
+  // Close browser
+  await browser.close();
+})();
